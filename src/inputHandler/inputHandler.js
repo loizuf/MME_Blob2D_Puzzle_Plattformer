6bcad24d0ff63a256e48c108dd218191d0a5e643
Jonathan <jon.brem@t-online.de>
2014-07-14 11:01:26 +0200

    input handler: Kommentare und KeyCodes eingefuegt

---
+++ src/inputHandler/inputHandler.js
@@ -0,0 +1,32 @@
+/*
+	The Input Handler takes all incoming Key Events (and !TODO! possibly XBox-/Controller-Inputs) and informs the Controller.
+
+
+*/
+BlobApp.InputHandler = (function() {
+	// public interface
+	var that = {},
+
+	// Contains key codes for the two blobs for the basic interaction: Movement, Triggers
+	var keyMap = {
+		p1Jump : 87,
+		p1Left : 65,
+		p1Right : 68,
+		p1Trigger : 83,
+
+		p2Jump : 38,
+		p2Left : 37,
+		p2Right : 39,
+		p2Trigger 40: 
+	},
+
+	init = function() {
+
+
+
+	};
+
+
+	that.init = init;
+	return that;
+})();
