f36ea907f7611d1ecdf873ca67f276f363079922
Jonathan <jon.brem@t-online.de>
2014-07-14 15:46:54 +0200

    Blob updated, work in progress

--- src/BlobApp/GameModel/Blob.js
+++ src/BlobApp/GameModel/Blob.js
@@ -1,6 +1,8 @@
 /*
 	This is a logical represantation of the blob (player character).
 	This is the super-class which contains everything both player need.
+
+	The blob gets most of the user input and tells the model / controller how to react.
 */
 BlobApp.BlobSuperClass = (function() {
 	var that = {},
@@ -13,9 +15,16 @@ BlobApp.BlobSuperClass = (function() {
 
 	// As the name suggests, 
 	jumpAllowed,
+	// Waiting for other blob to trigger a special interaction: No interaction allowed
+	waitingForOtherBlob,
+//!	 !Must be overriden by subclass!
+	specialSkills,
+	// If the blobs are currently doing some fancy interaction
+	specialInteractionActive
 
 	init = function(pX, pY, v, dir){
 		_setPropertiesOfBlob(pX, pY, v, dir);
+		return that;
 	},
 
 	_setPropertiesOfBlob = function(pX, pY, v, dir){
@@ -30,12 +39,64 @@ BlobApp.BlobSuperClass = (function() {
 		// Trigger something to kill other blob, wait some time, reset positions
 	},
 
-	// if jumpAllowed is true: Jump, else: do nothing
-	jump = function() {
+
+	_jump = function() {
 
 	},
 
+	/*
+		Tries to trigger a special event. 
+		If the other blob is ready, it might work; otherwhise, the other player is informed via a bubble-y-thing
 
+
+	*/
+	_triggerSpecial = function() {
+
+	},
+
+	// This if/else thing probably could be overriden in a better way. TODO
+	onLeftPressed = function() {
+		if(!specialInteractionActive) {
+			// move to the left
+		} else {
+
+		}
+	},
+
+		// This if/else thing probably could be overriden in a better way. TODO
+	onRightPressed = function() {
+		if(!specialInteractionActive) {
+			// move to the right
+		} else {
+			
+		}	
+	},
+
+	// if jumpAllowed is true: Jump, else: if specialInteraction: do stuff, else: do nothing
+	onUpPressed = function() {
+	/*	if(specialInteraction) {
+
+		} else if(jumpAllowed) {
+
+		} else {
+
+		} */
+	},
+
+	onDownPressed = function() {
+	/*	if(specialInteraction) {
+
+		} else {
+			_triggerSpecial();
+		}*/
+	};
+
+
+	that.onLeftPressed = onLeftPressed;
+	that.onRightPressed = onRightPressed;
+	that.onUpPressed = onUpPressed;
+	that.onDownPressed = onDownPressed;
 	that.init = init;
+
 	return that;
 })();
