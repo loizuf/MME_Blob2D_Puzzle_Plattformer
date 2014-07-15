780871bc810afd7d913e8972b77eb01cb1a0c768
Martin <martin.sellmeier@stud.uni-regensburg.de>
2014-07-15 13:27:47 +0200

    restructured entities

--- src/BlobApp/Entities/DynamicEntities/DynamicDoor.js
+++ src/BlobApp/Entities/DynamicEntities/DynamicDoor.js
@@ -1,4 +1,7 @@
 BlobApp.DynamicDoor = (function DynamicDoor(sprite, x_pos, y_pos, sizeX, sizeY, body) {
 	this.prototype = new DynamicEntity(sprite, x_pos, y_pos, sizeX, sizeY);
+	this.prototype.applyPhysicsBody = function() { 
+
+	};
 	this.body = body;
 });
--- src/BlobApp/Entities/DynamicEntities/Trap.js
+++ src/BlobApp/Entities/DynamicEntities/Trap.js
@@ -1,5 +1,8 @@
 BlopApp.Trap = (function Trap(sprite, x_pos, y_pos, sizeX, sizeY, trapID, body) {
 	this.prototype = new DynamicEntity(sprite, x_pos, y_pos, sizeX, sizeY);
+	this.prototype.applyPhysicsBody = function() { 
+
+	};
 	this.trapID = trapID;
 	this.body = body;
 });
