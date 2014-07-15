463873764a56480f90a0e75b982b09669c4df7dc
Soeren Nickel <loizuf.sn@gmail.com>
2014-07-15 13:29:54 +0200

    Static Entity contructor arguments added.

--- src/BlobApp/Entities/StaticEntity.js
+++ src/BlobApp/Entities/StaticEntity.js
@@ -1,4 +1,4 @@
-BlobApp.StaticEntity = (function StaticEntity(sprite) {
+BlobApp.StaticEntity = (function StaticEntity(sprite, x_pos, y_pos, sizeX, sizeY) {
 	this.prototype = new Entity(x_pos, y_pos, sizeX, sizeY);
 	this.prototype.applyPhysicsBody = function() { 
 
