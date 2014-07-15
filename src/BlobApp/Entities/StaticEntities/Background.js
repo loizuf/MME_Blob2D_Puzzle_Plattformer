c04e025da275e5f9b6d38ddfad9444eb086956bf
Eth3real <eth3real89@gmail.de>
2014-07-15 13:31:56 +0200

    rearrangement of source files

--- src/BlobApp/Entities/StaticEntities/Background.js
+++
@@ -1,7 +0,0 @@
-BlobApp.Platform = (function Platform(sprite, x_pos, y_pos, sizeX, sizeY, body) {
-	this.prototype = new StaticEntity(sprite, x_pos, y_pos, sizeX, sizeY);
-	this.prototype.applyPhysicsBody = function() { 
-
-	};
-	this.body = body;
-});
