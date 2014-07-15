cf0b5eb9b3239c945e946a6379d91f7ea4cf9b2f
Eth3real <eth3real89@gmail.de>
2014-07-15 13:43:07 +0200

    fixing typos and source file referencing in index.html

--- src/BlobApp/Entities/DynamicEntities/Trap.js
+++ src/BlobApp/Entities/DynamicEntities/Trap.js
@@ -1,4 +1,4 @@
-BlopApp.Trap = (function Trap(sprite, x_pos, y_pos, sizeX, sizeY, trapID, body) {
+BlobApp.Trap = (function Trap(sprite, x_pos, y_pos, sizeX, sizeY, trapID, body) {
 	this.prototype = new DynamicEntity(sprite, x_pos, y_pos, sizeX, sizeY);
 	this.prototype.applyPhysicsBody = function() { 
 
