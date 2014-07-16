300e9f77e21e0fc9616d8b1a1e11ee3462abe6d6
Martin <martin.sellmeier@stud.uni-regensburg.de>
2014-07-16 14:50:16 +0200

    added animation to blob

--- src/BlobApp/Entities/DynamicEntities/Blob.js
+++ src/BlobApp/Entities/DynamicEntities/Blob.js
@@ -1,6 +1,7 @@
 BlobApp.Blob = (function Blob(x_pos, y_pos, sizeX, sizeY, blobID) {
 
 	var that = this,
+	this.blobID = blobID,
 	sprite, tilesetSheet;
 
 
@@ -10,14 +11,14 @@ BlobApp.Blob = (function Blob(x_pos, y_pos, sizeX, sizeY, blobID) {
 		tileset = new Image();
 		var height;
 		if(blobID == EntityConfig.REDBLOBID){
-			tileset.src = "res/img/redblob.png"//mapData.tilesets[0].image;
+			tileset.src = "res/img/redBlobMoveRight.png"//mapData.tilesets[0].image;
 			height = 50;
 		}else{
 			tileset.src = "res/img/blob.png"//mapData.tilesets[0].image;
 			height = 25;
 		}
 		// getting imagefile from first tileset
-		
+		_listeners();
 		// callback for loading layers after tileset is loaded
 		tileset.onLoad = _initSprite(tileset, height);		
 	}
@@ -27,7 +28,13 @@ BlobApp.Blob = (function Blob(x_pos, y_pos, sizeX, sizeY, blobID) {
 			images : [ tileset ],
 			frames : {
 				width : 25,
-				height : h
+				height : h,
+				count: 20,
+				regX: width/2,
+				regY: height/2
+			}
+			animations: {
+
 			}
 		};
 		// create spritesheet for generic objects (ground e.g.)
@@ -35,20 +42,37 @@ BlobApp.Blob = (function Blob(x_pos, y_pos, sizeX, sizeY, blobID) {
 
 		sprite = new createjs.Sprite(tilesetSheet);
 
+		cellBitmap.gotoAndStop(0);
 		/* koordinaten kommen aus dem levelloader */
 		sprite.x = x_pos;
 		sprite.y = y_pos;
 
-		sprite.regX = 12;
-		sprite.regY = 12;
 		/* setzen auf höhe/2, breite /2 */
 		sprite.regX = imageData.frames.width/2;
 		sprite.regY = imageData.frames.height/2;
 		sprite.snapToPixel = true;
 		sprite.mouseEnabled = false;
 
-	}
+	},
 
+	_listeners = function(){
+		$('body').on('blobanimationChanged', _animate);
+	},
+
+	_animate = function(event, data){
+		if(this.blobID==data.blobID){
+			switch(data.animationKey){
+				case IDLE:
+					gotoAndPlay
+				break;
+				case MOVERIGHT:
+
+				break;
+				case MOVELEFT:
+				break;
+			}
+		}
+	}
 
 	this.prototype.init();
 
