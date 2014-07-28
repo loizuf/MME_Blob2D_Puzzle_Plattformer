c8ecb890d28399b75be4b4f87e8010ab8b35e283
Martin <martin.sellmeier@stud.uni-regensburg.de>
2014-07-28 14:58:58 +0200

    key displayed correctly

--- src/BlobApp/Entities/DynamicEntities/Key.js
+++ src/BlobApp/Entities/DynamicEntities/Key.js
@@ -1,7 +1,42 @@
 BlobApp.Key = (function Key(x_pos, y_pos, sizeX, sizeY, keyID) {
 	this.prototype = new BlobApp.DynamicEntity(sprite, x_pos, y_pos, sizeX, sizeY);
-	this.prototype.applyPhysicsBody = function() { 
 
-	};
+	var sprite, tilesetSheet;
+
+	this.prototype.init =function(){
+		var tileset = new Image();
+		tileset.src = "res/img/key.png"
+		// callback for loading sprite after tileset is loaded
+		tileset.onLoad = _initSprite(tileset, sizeX,sizeY);		
+	},	
+	
+
+	_initSprite = function(tileset, w,h){
+		var imageData = {
+			images : [ tileset ],
+			frames : {
+				width : w,
+				height : h,
+				count: 1
+			}
+		}
+
+		// create spritesheet for generic objects (ground e.g.)
+		tilesetSheet = new createjs.SpriteSheet(imageData);
+
+		sprite = new createjs.Sprite(tilesetSheet);
+
+		/* koordinaten kommen aus dem levelloader */
+		sprite.regX = w/2;
+		sprite.regY = h/2;
+		sprite.x = x_pos;
+		sprite.y = y_pos+12;
+
+		sprite.snapToPixel = true;
+		sprite.mouseEnabled = false;
+	},
+
+	this.prototype.init();
 	this.keyID = keyID;
+	this.sprite = sprite;
 });
