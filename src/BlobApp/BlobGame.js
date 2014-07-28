e27f98e1afbeb022d3d986a043cbeb1adf2764e4
Jonathan <jon.brem@t-online.de>
2014-07-28 11:20:42 +0200

    Doors are now correctly mapped to buttons

--- src/BlobApp/BlobGame.js
+++ src/BlobApp/BlobGame.js
@@ -2,13 +2,11 @@ BlobApp.BlobGame = (function() {
 	var that = {},
 
 	init = function(){
-		console.log("initiated");
 		var $menuplay = $('#menu-play');
 		$menuplay.on('click',startGame);
 	},
 
 	startGame = function(){
-		console.log("started game");
 		var $menucontainer = $('#menu-container');
 		var $gamecanvas = $('#canvas-container');
 		$gamecanvas.css('display', 'block');
