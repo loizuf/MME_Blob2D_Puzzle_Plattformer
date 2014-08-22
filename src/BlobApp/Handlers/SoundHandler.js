f8c3317f3385046a3d0406522824d893319df58f
Martin <martin.sellmeier@stud.uni-regensburg.de>
2014-08-22 14:33:23 +0200

    sound handler todos added, still throws error, trying to resolve

--- src/BlobApp/Handlers/SoundHandler.js
+++ src/BlobApp/Handlers/SoundHandler.js
@@ -3,29 +3,37 @@
 
 
 BlobApp.SoundHandler = (function() {
-    // public interface
+    
     var that = {},
 
 
     init = function() {
         var assetsPath = "res/sound/";
+        //TODO: add files here, id is given manually
         manifest = [
             {id:"test", src:"test.ogg"},
         ];
 
-    //    createjs.Sound.alternateExtensions = ["mp3"];
+        //createjs.Sound.alternateExtensions = ["mp3"];
+         //TODO: maybe exract this to a preload module
         preload = new createjs.LoadQueue(true, assetsPath);
         preload.installPlugin(createjs.Sound);
-        preload.addEventListener("complete", doneLoading);
+        preload.addEventListener("complete", _doneLoading);
         preload.loadManifest(manifest);
+
+        _listeners();
+
         return that;
     },
 
-    doneLoading = function() {
-        console.log("woop");
-        // start the music
-        createjs.Sound.play("test", {interrupt:createjs.Sound.INTERRUPT_NONE, loop:-1, volume:0.4});
+    _doneLoading = function() {
+          createjs.Sound.play("test", {interrupt:createjs.Sound.INTERRUPT_NONE, loop:-1, volume:0.4});
+
     };
+
+    _listeners = function(){
+          //TODO: register events fired for the sound handler
+    }
 
     that.init = init;
     return that;
