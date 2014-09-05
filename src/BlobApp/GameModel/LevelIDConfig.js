b493d691d1350804889b54a21ea9ae7e37dbef14
Soeren <loizuf.sn@gmail.com>
2014-09-05 14:49:09 +0200

    Overworlds changed

---
+++ src/BlobApp/GameModel/LevelIDConfig.js
@@ -0,0 +1,62 @@
+LevelIDConfig = {
+	One = {
+		LvlID = 1,
+		OWID = 1
+	},
+	Two = {
+		LvlID = 2,
+		OWID = 1
+	},
+	Three = {
+		LvlID = 3,
+		OWID = 1
+	},
+	Four = {
+		LvlID = 4,
+		OWID = 1
+	},
+	Five = {
+		LvlID = 5,
+		OWID = 1
+	},
+	Six = {
+		LvlID = 1,
+		OWID = 2
+	},
+	Seven = {
+		LvlID = 2,
+		OWID = 2
+	},
+	Eight = {
+		LvlID = 3,
+		OWID = 2
+	},
+	Nine = {
+		LvlID = 4,
+		OWID = 2
+	},
+	Ten = {
+		LvlID = 5,
+		OWID = 2
+	},
+	Eleven = {
+		LvlID = 1,
+		OWID = 3
+	},
+	Twelve = {
+		LvlID = 2,
+		OWID = 3
+	},
+	Thirteen = {
+		LvlID = 3,
+		OWID = 3
+	},
+	Fourteen = {
+		LvlID = 4,
+		OWID = 3
+	},
+	Fifteen = {
+		LvlID = 5,
+		OWID = 3
+	}	
+}
