f0ba02605a57a851f72e1a2310908f5838fd77dc
Martin <martin.sellmeier@stud.uni-regensburg.de>
2014-09-19 21:56:48 +0200

    finised implementing hint handler, keyhints need to be reworked

--- src/BlobApp/Handlers/HintHandler.js
+++ src/BlobApp/Handlers/HintHandler.js
@@ -11,18 +11,19 @@ BlobApp.HintHandler = (function() {
 	_setHintClasses = function (p1Controller, p2Controller) {
 		if(p1Controller){
 			var $temp = $('.p1');
-			$temp.addClass('offset');
+			$temp.children().addClass('offset');
 		}
 		if(p2Controller){
 			var $temp = $('.p2');
-			$temp.addClass('offset');
+			$temp.children().addClass('offset');
 		}
 	},
 
 	showHints = function (numHints) {
 		var i;
+		console.log("shows "+numHints+" hints");
 
-		for(i = 0; i < numHints; i++){
+		for(i = 1; i <= numHints; i++){
 			var $temp = $('.ht-' +i );
 			$temp.removeClass('hidden');
 		}
